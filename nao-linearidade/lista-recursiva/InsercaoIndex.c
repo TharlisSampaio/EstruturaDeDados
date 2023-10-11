4e514d525dbc3481f1da4e26959b84a1801df01f
Tharlis Sampaio <tharlis.sampaio@outlook.com>
2023-10-10 22:23:31 -0500

    inserir um ponto em uma posição arbitraria

---
+++ nao-linearidade/lista-recursiva/InsercaoIndex.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct ponto{
+    float x;
+    float y;
+    struct ponto *prox; // alem te ter os pontos (x,y) terá um pronteiro aprondando para o proximo Struct
+};
+
+typedef struct ponto Ponto;
+
+Ponto *listaPontos; // este ponteiro ira apontar para o primeiro elemento da lista
+// a partir do primeiro irei acessar os demais
+
+int length = 0;
+
+void addIndex(float x, float y, int index){
+    Ponto *p = (Ponto*) malloc(sizeof(Ponto));
+    p->x = x;
+    p->y = y;
+
+    if(index>length){
+        printf("Posição invalida");
+    }else{
+        if(index==0){ // aponta o ponteiro inicia para o novo primeiro elemento
+            p->prox = listaPontos; // o antigo primeiro elemento passa a ser o segundo
+            listaPontos = p; // e o novo elemento passa a ser o primeiro
+        }else{
+            // vou pecorrer para adicionar em determinada posição
+            Ponto *listaAux = listaPontos;
+            int i=0;
+            while(i!=index-1){
+                listaAux = listaAux->prox;
+                i++;
+            }
+            p->prox = listaAux->prox;
+            listaAux->prox = p;
+        }
+        length++;
+    }
+}
+
+void imprime(Ponto *p){
+    if (p!=NULL)
+    {
+        printf("\nPont(%.2f,%.2f)",p->x, p->y);
+        imprime(p->prox); // aplicando recursão
+    }
+}
+
+int main(){
+
+    addIndex(1,1,0);
+    addIndex(2,9,1);
+    addIndex(4,3,2);
+    addIndex(6,7,2);
+    addIndex(0,0,0);
+
+    Ponto *auxLista = listaPontos;
+    imprime(auxLista);
+
+    return 0;
+}
